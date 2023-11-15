60f6cd43fb490207b260e1e5e124a1bec55820e8
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-11-15 14:25:52 -0300

    Update Mail.jsx

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -22,18 +22,20 @@ export const Mail = () => {
 
   return (
     <>
-      <Audio
-        height={80}
-        width={80}
-        radius={9}
-        color="#FB75C7"
-        ariaLabel="loading"
-        wrapperStyle={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      />
+      <div className="vh-100">
+        <Audio
+          height={80}
+          width={80}
+          radius={9}
+          color="#FB75C7"
+          ariaLabel="loading"
+          wrapperStyle={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+      </div>
       <div className="d-none">{orderLoaded && <Send order={order} />}</div>
     </>
   );
