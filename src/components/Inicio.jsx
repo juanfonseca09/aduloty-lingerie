5e9ca4a2e2f78d51eea74843f8919e335f727491
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-11-15 20:38:51 -0300

    Update Inicio.jsx

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -21,10 +21,11 @@ export const Inicio = () => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get('/products?page=1&limit=8&category="Lencería"');
-        console.log(res.data)
-        setProducts(res.data);
-        setIsLoading(false);
+        const res = await axios.get('/products?category="Lencería"');
+        if (res.data && res.data.length > 0) {
+          setProducts(res.data);
+          setIsLoading(false);
+        } 
       } catch (err) {
       }
     };
