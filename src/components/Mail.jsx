c3f022c8ebf81989ae2c86f0cefb165a87895b62
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-11-15 14:20:27 -0300

    Update Mail.jsx

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -2,27 +2,39 @@ import { useSelector } from "react-redux";
 import { Send } from "./Send";
 import { useEffect, useState } from "react";
 import axios from "../axiosInstance";
+import { Audio } from "react-loader-spinner";
 
 export const Mail = () => {
   const cart = useSelector((state) => state.cart);
   const [order, setOrder] = useState({});
-  const [orderLoaded, setOrderLoaded] = useState(false); 
+  const [orderLoaded, setOrderLoaded] = useState(false);
 
   useEffect(() => {
     const getOrder = async () => {
       try {
-        const res = await axios.get("/orders/"+ cart.orderId);
+        const res = await axios.get("/orders/" + cart.orderId);
         setOrder(res.data);
-        setOrderLoaded(true); 
-      } catch (err) {
-      }
+        setOrderLoaded(true);
+      } catch (err) {}
     };
     getOrder();
   }, [cart.orderId]);
 
   return (
-    <div className="d-none">
-      {orderLoaded && <Send order={order} />}
-    </div>
+    <>
+      <Audio
+        height={80}
+        width={80}
+        radius={9}
+        color="#FB75C7"
+        ariaLabel="loading"
+        wrapperStyle={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
+      <div className="d-none">{orderLoaded && <Send order={order} />}</div>
+    </>
   );
 };
